1eb348a47f9a912c4b1c350494f2edb21559475c
Amit Dhamu <dev@amitd.co>
2015-08-06 12:52:08 +0100

    Initial Commit

---
+++ ShowHiddenFilesMenuBarApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ShowHiddenFilesMenuBarApp
+//
+//  Created by Amit on 06/08/2015.
+//  Copyright (c) 2015 Amit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
